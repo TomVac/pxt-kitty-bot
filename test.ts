b22520dce8b92ef879f87224a6cc4a9a3228fbab
TomVac <59280415+TomVac@users.noreply.github.com>
2020-01-05 13:17:45 +0100

    example

--- test.ts
+++ test.ts
@@ -1,1 +1,16 @@
-// tests go here; this will not be compiled when this package is used as a library
+input.onButtonPressed(Button.A, function () {
+    kittybot.do_action(kittybot.actions(kittybot.action_name.walk_boldly), 2, 100)
+})
+input.onButtonPressed(Button.B, function () {
+    kittybot.do_action(kittybot.actions(kittybot.action_name.walk_backward_boldly), 2, 100)
+})
+kittybot.set_offset(
+    75,
+    105,
+    95,
+    85
+)
+kittybot.stand_still()
+basic.forever(function () {
+
+})
